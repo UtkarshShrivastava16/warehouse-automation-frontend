98adbc44857887bf47b134f6c2a5ca1bb604243c
UtkarshShrivastava16 <ushri16@gmail.com>
2023-04-17 15:55:29 -0400

    new

--- src/apiConfig.js
+++
@@ -1,6 +0,0 @@
-export const apiConfig = {
-    resourceApi: {
-        baseUrl: "http://localhost:7071/api/",
-        scopes: ["api://warehouse-api/warehouse.write","api://warehouse-api/warehouse.read"],
-    },
-};
